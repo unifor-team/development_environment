edd5c59527ff0368adb1d7fbdfb2c5b87e043d46
BrenoOliveirads <brenooliveira.ds@gmail.com>
2023-05-23 19:24:02 -0300

    refactor: format document Class Client

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -6,66 +6,45 @@ public class Client extends User {
     private String nameCompany;
     private String buy;
 
-
-
-public Client (String name, Integer age, String address, Boolean fidelity, Double quantityMoney, Integer cnpj, String services, String nameCompany, String buy){
-     super(name, age, address, fidelity);
-     this.cnpj = cnpj;
-     this.services = services;
-     this.nameCompany = nameCompany;
-     this.buy = buy;
+    public Client(String name, Integer age, String address, Boolean fidelity, Double quantityMoney, Integer cnpj,
+            String services, String nameCompany, String buy) {
+        super(name, age, address, fidelity);
+        this.cnpj = cnpj;
+        this.services = services;
+        this.nameCompany = nameCompany;
+        this.buy = buy;
     }
 
+    public Integer getCnpj() {
+        return cnpj;
+    }
 
+    public void setCnpj(Integer cnpj) {
+        this.cnpj = cnpj;
+    }
 
-public Integer getCnpj() {
-    return cnpj;
-}
+    public String getServices() {
+        return services;
+    }
 
+    public void setServices(String services) {
+        this.services = services;
+    }
 
+    public String getNameCompany() {
+        return nameCompany;
+    }
 
-public void setCnpj(Integer cnpj) {
-    this.cnpj = cnpj;
-}
+    public void setNameCompany(String nameCompany) {
+        this.nameCompany = nameCompany;
+    }
 
+    public String getBuy() {
+        return buy;
+    }
 
-
-public String getServices() {
-    return services;
-}
-
-
-
-public void setServices(String services) {
-    this.services = services;
-}
-
-
-
-public String getNameCompany() {
-    return nameCompany;
-}
-
-
-
-public void setNameCompany(String nameCompany) {
-    this.nameCompany = nameCompany;
-}
-
-
-
-public String getBuy() {
-    return buy;
-}
-
-
-
-public void setBuy(String buy) {
-    this.buy = buy;
-}
-
-
-
-
+    public void setBuy(String buy) {
+        this.buy = buy;
+    }
 
 }
