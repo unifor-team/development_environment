1f541c0538100febf9e8d4979690920ea3519d99
Manelito <emanuelwork1@gmail.com>
2023-05-23 19:55:07 -0300

    feat: adding Product methods

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -9,4 +9,61 @@ public abstract class Product {
   private Category category;
   private Boolean hasPromotion;
   private Double discount;
+
+  public Product(String name, Integer quantity, Double price, Category category, Boolean hasPromotion, Double discount) {
+    this.name = name;
+    this.quantity = quantity;
+    this.price = price;
+    this.category = category;
+    this.hasPromotion = hasPromotion;
+    this.discount = discount;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
+  public Boolean getHasPromotion() {
+    return hasPromotion;
+  }
+
+  public void setHasPromotion(Boolean hasPromotion) {
+    this.hasPromotion = hasPromotion;
+  }
+
+  public Double getDiscount() {
+    return discount;
+  }
+
+  public void setDiscount(Double discount) {
+    this.discount = discount;
+  }
 }
